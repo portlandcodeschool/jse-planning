fc7ca72e8b553a78c54ea73287ed3da4a24aacc5
Dan Bauer <dsbauer@gmail.com>
2014-09-24 15:00:57 -0700

    First draft

---
+++ cards3-template.js
@@ -0,0 +1,99 @@
+function makeCard(id) {
+    // If id is invalid (out of range, etc)
+    return null;
+
+    // Otherwise build an instance object with an id property,
+    // representing one card, and attach to it four methods:
+    //   rank()
+    //   suit()
+    //   color()
+    //   name()
+    // Each method should be a just reference to the corresponding method
+    //  of the factory itself.
+
+    return /* that instance here */;
+}
+
+//-----------------------
+// Methods to be called through factory:
+//-----------------------
+
+makeCard.isCard = function(card) { // --> true,false
+    // return true if card is a valid card instance made by this factory
+
+}
+
+//-----------------------------
+// Methods called though instances (where 'this' means the instance):
+//-----------------------------
+
+makeCard.rank = function() { // --> 1..13, NaN
+    // code here...
+};
+
+makeCard.suit = function() { // --> 1..4, NaN
+    // code here...
+};
+   
+makeCard.color = function() { // -->"red,"black",NaN
+    // code here...
+};
+
+makeCard.cardName = function() { //--> string, NaN
+    // This method can't have the key 'name' within the makeCard function,
+    // but instance objects can store a reference to it called 'name'
+
+    // code here...
+};
+
+    
+
+
+// Testing suite...
+function assert(claim,message) {
+    if (!claim) console.error(message);
+}
+
+// card instances needed for assertions:
+var card0 = makeCard(0);
+var card3 = makeCard(3);
+var card5 = makeCard(5);
+var card51 = makeCard(51);
+
+
+// Test instance methods:
+assert(card0.rank()===1,  "Test 1 failed");
+assert(card3.rank()===1,  "Test 2 failed");
+assert(card51.rank()===13,"Test 3 failed");
+assert(card0.suit()===1,  "Test 4 failed");
+assert(card5.suit()===2,  "Test 5 failed");
+assert(card51.suit()===4, "Test 6 failed");
+assert(card0.color()==='red',   "Test 10 failed");
+assert(card3.color()==='black', "Test 11 failed");
+assert(card5.name()==='Two of Diamonds', "Test 12 failed");
+assert(card51.name()==='King of Clubs',  "Test 13 failed");
+
+// Test makeCard.isCard:
+assert(makeCard.isCard(card0),  "Test 21 failed")
+assert(makeCard.isCard(card51), "Test 22 failed")
+assert(!makeCard.isCard(0)),    "Test 23 failed")
+assert(!makeCard.isCard({})),   "Test 24 failed")
+
+
+// Test card-making results:
+assert(!makeCard(52),"Test 26 failed");
+assert(!makeCard("0"),"Test 27 failed");
+assert(!makeCard(-1),"Test 28 failed");
+assert(!makeCard(false),"Test 30 failed");
+assert(!makeCard(true),"Test 31 failed");
+
+
+// Test that methods are shared:
+assert(card0 !== card3, "Test 50 failed"); //first prove different cards
+assert(card0.rank === card3.rank, "Test 51 failed");
+assert(card0.suit === card3.suit, "Test 52 failed");
+assert(card0.name === card3.name, "Test 53 failed");
+//etc...
+
+
+
